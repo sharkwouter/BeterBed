b239271a77c5e45478d899b66eeae4c61bf4db39
Wouter Wijsman <wwijsman@live.nl>
2019-07-14 23:08:41 +0200

    Removed the feature which would hide armor It was too risky and had too many edge cases

--- README.md
+++ README.md
@@ -4,8 +4,5 @@ A minecraft plugin which makes beds better. It adds the following functions:
 
 - Players are fully healed after using a bed.
 - Beds can be used to set your spawn point and heal during the day.
-- Armor and weapons are hidden when laying in a bed.
-
-**note:** if the server crashes while a player is laying in a bed, the equipped armor and weapons may be lost. Stopping the server normally does not cause this issue.
 
 ![](https://github.com/sharkwouter/BeterBed/raw/master/BeterBed.png)
--- src/nl/wiselan/beterbed/Main.java
+++ src/nl/wiselan/beterbed/Main.java
@@ -7,22 +7,11 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerBedEnterEvent.BedEnterResult;
 import org.bukkit.event.player.PlayerBedLeaveEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import org.bukkit.attribute.Attribute;
 
 public class Main extends JavaPlugin implements Listener {
-
-	//Hashmaps used for temporarily saving player items
-	static HashMap<Player, ItemStack[]> armorHashMap = new HashMap<Player, ItemStack[]>();
-	static HashMap<Player, ItemStack> mainhandHashmap = new HashMap<Player, ItemStack>();
-	static HashMap<Player, ItemStack> offhandHashmap = new HashMap<Player, ItemStack>();
-
 
 	@Override
 	public void onEnable() {
@@ -31,9 +20,7 @@ public class Main extends JavaPlugin implements Listener {
 
 	@Override
 	public void onDisable() {
-		for(Map.Entry<Player, ItemStack[]> entry : armorHashMap.entrySet()){
-		    restoreItems(entry.getKey(), false);
-		}
+		
 	}
 
 	/*
@@ -42,12 +29,8 @@ public class Main extends JavaPlugin implements Listener {
 	@EventHandler
 	public void OnPlayerBedEnterEvent(PlayerBedEnterEvent e) {
 		BedEnterResult result = e.getBedEnterResult();
-		
-		if (result == BedEnterResult.OK) {
-			Player player = e.getPlayer();
-			saveItems(player);
-			removeItems(player);
-		} else if (result == BedEnterResult.NOT_POSSIBLE_NOW) {
+	
+		if (result == BedEnterResult.NOT_POSSIBLE_NOW) {
 			//Allow players to use beds during the day
 			e.setUseBed(Result.ALLOW);
 		}
@@ -62,55 +45,5 @@ public class Main extends JavaPlugin implements Listener {
 
 		//Heal the player to full
 		player.setHealth(player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
-
-		restoreItems(player);
-	}
-
-	private void saveItems(Player player) {
-		PlayerInventory inventory = player.getInventory();
-
-		//Save the armor and weapons the player has equipped
-		armorHashMap.put(player,  inventory.getArmorContents());
-		mainhandHashmap.put(player, inventory.getItemInMainHand());
-		offhandHashmap.put(player, inventory.getItemInOffHand());
-	}
-
-	private void removeItems(Player player) {
-		PlayerInventory inventory = player.getInventory();
-
-		//Remove armor and weapons from the player
-		inventory.setArmorContents(new ItemStack[]{});
-		inventory.setItemInMainHand(null);
-		inventory.setItemInOffHand(null);
-	}
-
-	private void restoreItems(Player player, boolean cleanup) {
-		PlayerInventory inventory = player.getInventory();
-
-		//Return the items taken from the player when entering the bed
-		//Give the player their armor back
-		ItemStack[] armor = (ItemStack[]) armorHashMap.get(player);
-		inventory.setArmorContents(armor);
-
-		//Give the player their main hand item back
-		ItemStack mainhand = (ItemStack) mainhandHashmap.get(player);
-		inventory.setItemInMainHand(mainhand);
-
-		//Give the player their off hand item back
-		ItemStack offhand = (ItemStack) offhandHashmap.get(player);
-		inventory.setItemInOffHand(offhand);
-
-		//Has to be seperate to prevent the function from deleting entries while looping
-		if (cleanup) {
-			//Remove the entries from the hashmap
-			armorHashMap.remove(player);
-			mainhandHashmap.remove(player);
-			offhandHashmap.remove(player);
-		}
-	}
-
-	//This function is used when not looped
-	private void restoreItems(Player player) {
-		restoreItems(player, true);
 	}
 }
